45f671e4e7811d9863ee80a1d6359ae43f38eb7e
pueppiblue <nils.freigang@projektmotor.de>
2022-10-16 15:05:04 +0200

    add form for adding replog data

--- web/assets/js/RepLogApp/RepLogApp.js
+++ web/assets/js/RepLogApp/RepLogApp.js
@@ -28,12 +28,6 @@ export default class RepLogApp extends Component {
                     </tr>
                     </thead>
                     <tbody>
-                    <tr>
-                        <td>Big Fat Cat</td>
-                        <td>10</td>
-                        <td>180</td>
-                        <td>...</td>
-                    </tr>
                     {
                         repLogs.map((replog) =>
                             (
@@ -54,6 +48,37 @@ export default class RepLogApp extends Component {
                     </tr>
                     </tfoot>
                 </table>
+                <form className="form-inline" style={{display: "flex", justifyContent: "space-between"}}>
+                    <div className="form-group">
+                        <label className="sr-only control-label required" htmlFor="rep_log_item">
+                            What did you lift?
+                        </label>
+                        <select id="rep_log_item"
+                                name="item"
+                                required="required"
+                                className="form-control"
+                                defaultValue={""}
+                        >
+                            <option value="">What did you lift?</option>
+                            <option value="cat">Cat</option>
+                            <option value="fat_cat">Big Fat Cat</option>
+                            <option value="laptop">My Laptop</option>
+                            <option value="coffee_cup">Coffee Cup</option>
+                        </select>
+                    </div>
+
+                    <div className="form-group">
+                        <label className="sr-only control-label required" htmlFor="rep_log_reps">
+                            How many times?
+                        </label>
+                        <input type="number" id="rep_log_reps"
+                               name="reps" required="required"
+                               placeholder="How many times?"
+                               className="form-control" />
+                    </div>
+
+                    <button type="submit" className="btn btn-primary">I Lifted it!</button>
+                </form>
 
             </div>
         );
